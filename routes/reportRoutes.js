8919a650652df3219d4a7cbc54981909b7e43fcc
Thomas <thomas.lapere@supinfo.com>
2025-04-30 18:19:36 +0200

    statreport

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -1,20 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const reportController = require('../controllers/reportController');
-const reportStatsController = require('../controllers/reportStatsController');
-const duplicateToStats = require('../middleware/reportStatsMiddleware');
-
-// Appliquer le middleware avant les routes
-router.use(duplicateToStats);
 
 router.post('/reports', reportController.createReport);
 
 router.get('/reports', reportController.getNearbyReports);
 
 router.get('/reports/clusters', reportController.getReportClusters);
-
-// Nouvelle route pour les stats
-router.get('/reports/stats', reportStatsController.getAllReports);
 
 router.post('/reports/:reportId/upvote', reportController.upvoteReport);
 
