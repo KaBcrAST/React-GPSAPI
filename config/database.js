ebf2bf15db1403f770567a5136191dae1086d162
thomas lapere <thomas.lapere@supinfo.com>
2025-05-07 17:48:24 +0200

    bddsec

--- config/database.js
+++ config/database.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb+srv://thomaslaperesupinfo:ROwAq6izugTXfaTd@cluster0.f07klyp.mongodb.net/werego', {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       serverSelectionTimeoutMS: 5000,
