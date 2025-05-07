e816053459c8d3931c5bdf1227c91a505411ca37
Thomas <thomas.lapere@supinfo.com>
2025-05-07 11:48:46 +0200

    alerte

--- routes/navigationRoutes.js
+++ routes/navigationRoutes.js
@@ -11,5 +11,7 @@ router.get('/speed-limit', speedLimitController.getSpeedLimit);
 router.get('/preview', navigationController.getRoutePreview);
 router.get('/route-without-tolls', navigationController.getRouteWithoutTolls);
 router.get('/navigation-steps', navigationStepsController.getNavigationSteps);
+// Ajoutez la route pour obtenir directement les incidents
+router.get('/incidents', navigationController.getTrafficIncidents);
 
 module.exports = router;
