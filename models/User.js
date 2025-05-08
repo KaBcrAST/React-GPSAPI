af9a4d5d3f309b69a74dd48ef500ba6324ad8d35
thomas lapere <thomas.lapere@supinfo.com>
2025-05-08 12:22:57 +0200

    usermodel

--- models/User.js
+++ models/User.js
@@ -21,6 +21,11 @@ const userSchema = new mongoose.Schema({
   },
   googleId: String,
   picture: String,
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user'
+  },
   lastLogin: {
     type: Date,
     default: Date.now
